f8870ca3dc82653b58622d9b3437495fcee71cdc
Pedro Henrique <pedro12897@gmail.com>
2020-02-26 16:55:43 -0300

    conflitos resolvidos

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <php?
 
 <<<<<<< HEAD
-	echo "Olá mundo teste";
-=======
-	echo "Olá mundo!!!";
-      
-      	echo"Ola Hcode";
->>>>>>> bb22d0e3a37e5902c482cb6f5e8461a41913857b
+	echo "Olá mundo!";
+
+    echo"Ola Hcode";
 
 ?>
